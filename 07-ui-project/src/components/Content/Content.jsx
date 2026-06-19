bc8f8dfa46746e0c79b8de4587db5e4f35032e0c
Anuja <mestryanuja@gmail.com>
2026-06-19 17:38:20 +0100

    added scroll

--- 07-ui-project/src/components/Content/Content.jsx
+++ 07-ui-project/src/components/Content/Content.jsx
@@ -31,6 +31,36 @@ const cardArr = [
       level: "Senior level",
       rate: "$180/hr",
       location: "San Francisco, USA"
+    },
+    {
+      image: "https://images.unsplash.com/photo-1779126931857-f12866cf7049?q=80&w=1287&auto=format&fit=crop",
+      company: "Amazon",
+      postedAgo: "5 days ago",
+      title: "Designer",
+      type: "Part-time",
+      level: "Senior level",
+      rate: "$120/hr",
+      location: "Mumbai, India"
+    },
+    {
+      image: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?q=80&w=1287&auto=format&fit=crop",
+      company: "Google",
+      postedAgo: "2 days ago",
+      title: "Frontend Engineer",
+      type: "Full-time",
+      level: "Mid level",
+      rate: "$150/hr",
+      location: "New York, USA"
+    },
+    {
+      image: "https://images.unsplash.com/photo-1497366216548-37526070297c?q=80&w=1287&auto=format&fit=crop",
+      company: "Meta",
+      postedAgo: "1 day ago",
+      title: "Product Manager",
+      type: "Full-time",
+      level: "Senior level",
+      rate: "$180/hr",
+      location: "San Francisco, USA"
     }
   ]
 const Content = () => {
@@ -49,9 +79,9 @@ const Content = () => {
 
             <div className='text-8xl'><i class="ri-arrow-right-up-line"></i></div>
         </div>
-        <div className="content-right flex justify-around align-middle h-full w-7/12 m-3">
+        <div className="content-right flex justify-around align-middle h-full w-7/12 m-3 overflow-x-auto scrollbar-thin">
             <div>
-            <div className="flex gap-4 flex-wrap">
+            <div className="flex gap-4 ">
             {cardArr.map((elem, index) => (
                     <Card
                     key={index}
